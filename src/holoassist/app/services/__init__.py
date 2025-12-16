6edfabb068f30575b22855a69f3e77cf8e78a0ed
LeonCheung033 <leon.loeng.cheung@gmail.com>
2025-12-16 15:05:07 +0800

    chore: 更新services/__init__.py导出ConversationService

--- src/holoassist/app/services/__init__.py
+++ src/holoassist/app/services/__init__.py
@@ -3,6 +3,7 @@
 导出所有服务类，方便外部导入使用。
 """
 
+from holoassist.app.services.conversation_service import ConversationService
 from holoassist.app.services.deepseek_service import DeepSeekService
 from holoassist.app.services.embedding_service import EmbeddingService
 from holoassist.app.services.indexing_service import IndexingService
@@ -12,6 +13,7 @@ from holoassist.app.services.redis_semantic_cache import RedisSemanticCache
 from holoassist.app.services.siliconflow_service import SiliconFlowService
 
 __all__ = [
+    "ConversationService",
     "DeepSeekService",
     "EmbeddingService",
     "IndexingService",
