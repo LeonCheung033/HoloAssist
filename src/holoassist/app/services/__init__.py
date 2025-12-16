ac4d2b77ddfeb50e11112380cf3a1b1978b07c06
LeonCheung033 <leon.loeng.cheung@gmail.com>
2025-12-16 16:09:19 +0800

    feat: export SearchService and ToolRegistry

--- src/holoassist/app/services/__init__.py
+++ src/holoassist/app/services/__init__.py
@@ -6,19 +6,24 @@
 from holoassist.app.services.conversation_service import ConversationService
 from holoassist.app.services.deepseek_service import DeepSeekService
 from holoassist.app.services.embedding_service import EmbeddingService
+from holoassist.app.services.function_tools import FunctionTool, ToolRegistry
 from holoassist.app.services.indexing_service import IndexingService
 from holoassist.app.services.llm_factory import LLMFactory, get_factory
 from holoassist.app.services.ollama_service import OllamaService
 from holoassist.app.services.redis_semantic_cache import RedisSemanticCache
+from holoassist.app.services.search_service import SearchService
 from holoassist.app.services.siliconflow_service import SiliconFlowService
 
 __all__ = [
     "ConversationService",
     "DeepSeekService",
     "EmbeddingService",
+    "FunctionTool",
     "IndexingService",
     "OllamaService",
+    "SearchService",
     "SiliconFlowService",
+    "ToolRegistry",
     "LLMFactory",
     "get_factory",
     "RedisSemanticCache",
